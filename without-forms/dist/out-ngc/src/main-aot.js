4da4d25457702e3e4d34ca574940180a531e2ccf
dmitrysteblyuk <dmitry.steblyuk@gmail.com>
2017-04-12 11:11:57 +0300

    with/without forms module apps added + dist and compiled files

---
+++ without-forms/dist/out-ngc/src/main-aot.js
@@ -0,0 +1,9 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../dist/aot/src/app/app.module.ngfactory';
+import { environment } from './environments/environment';
+if (environment.production) {
+    enableProdMode();
+}
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+//# sourceMappingURL=main-aot.js.map
---
+++ without-forms/rollup-config.js
@@ -0,0 +1,33 @@
+import rollup      from 'rollup'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs';
+import uglify      from 'rollup-plugin-uglify'
+
+export default {
+  entry: 'without-forms/dist/out-ngc/src/main-aot.js',
+  dest: 'without-forms/dist/build-aot/build.js', // output a single application bundle
+  sourceMap: true,
+  sourceMapFile: 'without-forms/dist/build-aot/build.js.map',
+  format: 'iife',
+  treeshake: true,
+  onwarn: function(warning) {
+    // Skip certain warnings
+
+    // should intercept ... but doesn't in some rollup versions
+    if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
+    // intercepts in some rollup versions
+    if ( warning.indexOf && warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
+
+    // console.warn everything else
+    console.warn( warning.message );
+  },
+  plugins: [
+      nodeResolve({jsnext: true, module: true}),
+      commonjs({
+        include: [
+          'node_modules/rxjs/**'
+        ]
+      }),
+      uglify()
+  ]
+}
